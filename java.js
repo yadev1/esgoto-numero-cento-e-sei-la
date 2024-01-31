0f6a6309e46eccc3f40fb4ed7107963d94571685
yadev1 <134545307+yadev1@users.noreply.github.com>
2024-01-31 18:29:52 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,46 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function clearCanvas() {
+
+    background("white");
+}
+
+function draw() {
+
+    strokeWeight(13);
+
+    stroke(0);
+
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    var result = results[0].label;
+    document.getElementById('label').innerHTML = 'Nome:' + result.replace('_', ' ');
+
+    document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(result.replace('_', ' '));
+    synth.speak(utterThis);
+}
